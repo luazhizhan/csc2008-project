b62497836d7e2fd26d94007fff9b177d70e6cce3
Lua Zhi Zhan <zhizhanlua@gmail.com>
2023-02-24 18:36:05 +0800

    Add mongodb import script

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # csc2008-project
+
+## Setup
+
+### MySQL
+
+Download and install [mysql-installer-community-<version>.msi](https://dev.mysql.com/downloads/installer/)
+
+#### Import via SQL Scripts
+
+Install the following VSCode extensions.
+
+- [sqltools](https://marketplace.visualstudio.com/items?itemName=mtxr.sqltools)
+- [SQLTools MySQL/MariaDB](https://marketplace.visualstudio.com/items?itemName=mtxr.sqltools-driver-mysql)
+
+Execute `Script/create_table.sql` on MySQL Workbench or sqltools extension.
+
+Execute `Script/import_csv.sql` on sqltools extension.
+
+#### Import via SQL Dump
+
+Unzip and import `SQL dump` folder uploaded in the telegram group chat via MySQL Workbench Import Data function.
+
+### MonogoDB
+
+Install software.
+
+- [MongoDB](https://www.mongodb.com/try/download/community)
+- [Compass](https://www.mongodb.com/try/download/compass)
+
+#### Import via Dump
+
+Unzip and import `csc2008-mongodb-dump` folder uploaded in the telegram group using this command.
+
+```bash
+mongorestore --authenticationDatabase=admin -u <ADMIN> -p <PASSWORD> -d csc2008 <PATH>
+```
---
+++ migrate_mongodb_script.py
@@ -0,0 +1,118 @@
+import mysql.connector
+import pymongo
+import datetime
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+begin_time = datetime.datetime.now()
+print(f"{bcolors.HEADER}Script started at: {begin_time} {bcolors.ENDC}")
+
+delete_existing_documents = True
+mysql_host = "localhost"
+mysql_database = "csc2008"
+mysql_schema = "csc2008"
+mysql_user = "root"
+mysql_password = "password123"
+
+mongodb_host = "mongodb://admin:asdqwe123@localhost:27017/"
+mongodb_dbname = "csc2008"
+
+print(f"{bcolors.HEADER}Initializing database connections...{bcolors.ENDC}")
+print("")
+
+# MySQL connection
+print(f"{bcolors.HEADER}Connecting to MySQL server...{bcolors.ENDC}")
+mysqldb = mysql.connector.connect(
+    host=mysql_host,
+    database=mysql_database,
+    user=mysql_user,
+    password=mysql_password
+)
+print(f"{bcolors.HEADER}Connection to MySQL Server succeeded.{bcolors.ENDC}")
+
+# MongoDB connection
+print(f"{bcolors.HEADER}Connecting to MongoDB server...{bcolors.ENDC}")
+myclient = pymongo.MongoClient(mongodb_host)
+mydb = myclient[mongodb_dbname]
+print(f"{bcolors.HEADER}Connection to MongoDB Server succeeded.{bcolors.ENDC}")
+
+print(f"{bcolors.HEADER}Database connections initialized successfully.{bcolors.ENDC}")
+
+# Start migration
+print(f"{bcolors.HEADER}Migration started...{bcolors.ENDC}")
+dblist = myclient.list_database_names()
+if mongodb_dbname in dblist:
+    print(f"{bcolors.OKBLUE}The database exists.{bcolors.ENDC}")
+else:
+    print(f"{bcolors.WARNING}The database does not exist, it is being created.{bcolors.ENDC}")
+
+# Function migrate_table
+
+
+def migrate_table(db, col_name):
+    mycursor = db.cursor(dictionary=True)
+    mycursor.execute("SELECT * FROM " + col_name + ";")
+    myresult = mycursor.fetchall()
+
+    # map myresult date data to mongodb date format
+    for row in myresult:
+        for key in row:
+            if isinstance(row[key], datetime.date):
+                row[key] = datetime.datetime.combine(
+                    row[key], datetime.datetime.min.time())
+
+    collection = mydb[col_name]
+
+    if delete_existing_documents:
+        # delete all documents in the collection
+        collection.delete_many({})
+
+    # insert the documents
+    if len(myresult) > 0:
+        x = collection.insert_many(myresult)
+        return len(x.inserted_ids)
+    else:
+        return 0
+
+
+# Iterate through the list of tables in the schema
+table_list_cursor = mysqldb.cursor()
+table_list_cursor.execute(
+    "SELECT table_name FROM information_schema.tables WHERE table_schema = %s ORDER BY table_name LIMIT 15;", (mysql_schema,))
+tables = table_list_cursor.fetchall()
+
+total_count = len(tables)
+success_count = 0
+fail_count = 0
+
+for table in tables:
+    try:
+        print(f"{bcolors.OKCYAN}Processing table: {table[0]}...{bcolors.ENDC}")
+        inserted_count = migrate_table(mysqldb, table[0])
+        print(
+            f"{bcolors.OKGREEN}Processing table: {table[0]} completed. {inserted_count} documents inserted.{bcolors.ENDC}")
+        success_count += 1
+    except Exception as e:
+        print(f"{bcolors.FAIL} {e} {bcolors.ENDC}")
+        fail_count += 1
+
+print("")
+print("Migration completed.")
+print(f"{bcolors.OKGREEN}{success_count} of {total_count} tables migrated successfully.{bcolors.ENDC}")
+if fail_count > 0:
+    print(f"{bcolors.FAIL}Migration of {fail_count} tables failed. See errors above.{bcolors.ENDC}")
+
+end_time = datetime.datetime.now()
+print(f"{bcolors.HEADER}Script completed at: {end_time} {bcolors.ENDC}")
+print(f"{bcolors.HEADER}Total execution time: {end_time-begin_time} {bcolors.ENDC}")

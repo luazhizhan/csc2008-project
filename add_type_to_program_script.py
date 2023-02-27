d20d0ae67943eae2107071ac07fa7d97fa11b2eb
Lua Zhi Zhan <zhizhanlua@gmail.com>
2023-02-27 10:44:27 +0800

    Fix program migration script

--- add_type_to_program_script.py
+++ add_type_to_program_script.py
@@ -16,9 +16,9 @@ with open(input_file, 'r', encoding='utf8') as f_in, open(output_file, 'w', newl
     # iterate over each row of input file and add new column
     for row in reader:
         if 'min' in row['duration']:  # check if 'min' is in the 'duration' field
-            row['type'] = 'TV Show'  # add new column with value 'TV Show'
+            row['type'] = 'Movie'  # add new column with value 'TV Show'
         else:
-            row['type'] = 'Movie'  # add new column with value 'Movie'
+            row['type'] = 'TV Show'  # add new column with value 'Movie'
 
         # remove non-numeric characters
         duration = ''.join(filter(str.isdigit, row['duration']))
